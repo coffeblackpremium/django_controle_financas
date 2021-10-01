290960176f942fe04f6e4849da6ee0c16f1d00ad
João Damaceno <joaodamaceno188@gmail.com>
2021-09-30 22:39:38 -0300

    Adicionado uma nova página em Views e Feitos as migracoes iniciais no banco SQLITE

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
+
+def home(request):
+    return render(request,'usuarios/home.html')
+
+def login(request):
+    pass
